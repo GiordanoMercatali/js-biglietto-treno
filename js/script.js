62d5fa0ed27adfd13884b08dd2a5c3438edcbb03
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2023-10-19 09:56:32 +0200

    add: now shows the amount of discount

--- js/script.js
+++ js/script.js
@@ -9,16 +9,19 @@ const travelBaseCost = travelDistance * 0.21.toFixed(2);
 console.log(travelBaseCost, typeof travelBaseCost);
 
 let travelFinalCost = "";
+let discountAmount = "";
 
 if (userAge >= 18 && userAge <= 65){
     travelFinalCost = travelBaseCost;
     // console.log(`The cost of your travel is ${travelBaseCost}€`);
 } else if(userAge < 18){
     travelFinalCost = travelBaseCost * 0.8;
+    discountAmount = travelBaseCost * 0.2;
     // const youngDiscount = travelBaseCost * 0.8.toFixed(2);
     // console.log(`Minors get a 20% discount. The cost of your travel is ${youngDiscount}€`);
 } else{
     travelFinalCost = travelBaseCost * 0.6;
+    discountAmount = travelBaseCost * 0.4;
     // const oldDiscount = travelBaseCost * 0.6.toFixed(2);
     // console.log(`Old people get a 40% discount. The cost of your travel is ${oldDiscount}€`);
 }
@@ -30,9 +33,9 @@ let costReturn = "";
 if (userAge >= 18 && userAge <= 65){
     costReturn = `The cost of your travel is ${travelFinalCost.toFixed(2)}€`;
 } else if(userAge < 18){
-    costReturn = `Minors get a 20% discount. The cost of your travel is ${travelFinalCost.toFixed(2)}€`;
+    costReturn = `Minors get a ${discountAmount.toFixed(2)}€ discount. The cost of your travel is ${travelFinalCost.toFixed(2)}€`;
 } else{
-    costReturn = `Old people get a 40% discount. The cost of your travel is ${travelFinalCost.toFixed(2)}€`;
+    costReturn = `Old people get a ${discountAmount.toFixed(2)}€ discount. The cost of your travel is ${travelFinalCost.toFixed(2)}€`;
 }
 
 //Output
